33abad51c4ad4032278299c0b580e00f0fe43643
Samer Alshaer <rog.developer1@gmail.com>
2023-05-11 01:57:05 +0300

    fix slider

--- resources/views/landing_page/scripts.blade.php
+++ resources/views/landing_page/scripts.blade.php
@@ -62,22 +62,37 @@
         // Optional parameters
         direction: 'horizontal',
         loop: true,
-
         // If we need pagination
         pagination: {
             el: '.swiper-pagination',
         },
-
         // Navigation arrows
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
 
-        // And if we need scrollbar
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
+        // 4 slides per view
+        slidesPerView: 4,
+        spaceBetween: 30,
+        // Responsive breakpoints
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            // when window width is >= 480px
+            480: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            // when window width is >= 640px
+            640: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            }
+        }
     });
 </script>
 
